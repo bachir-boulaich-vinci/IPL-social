7da4757e22f83cf36845bd4642395f2fea0ba59e
bachir-boulaich-vinci <bachir.boulaich@student.vinci.be>
2025-01-17 16:41:42 +0100

    add: When length equals to 5

--- spec/passwordChecker.spec.js
+++ spec/passwordChecker.spec.js
@@ -15,4 +15,9 @@ describe("Password Checker: check if the password is valid", () => {
         const result = passwordCheck("111111");
         expect(result).toBe(false);
     });
+
+    it("Password: When length equals to 5", function() {
+        const result = passwordCheck("11111");
+        expect(result).toBe(false);
+    });
 });
--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,5 +1,5 @@
 function passwordCheck(password) {
-    if(password.length === 7 || password.length === 6) return false;
+    if(password.length < 8) return false;
 
     return true;
 }
