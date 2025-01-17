e741083000fdb6b0baa1ec20df71e5c7d629dc28
bachir-boulaich-vinci <bachir.boulaich@student.vinci.be>
2025-01-17 16:50:05 +0100

    add: test when password do not contain any special character

--- spec/passwordChecker.spec.js
+++ spec/passwordChecker.spec.js
@@ -2,22 +2,32 @@ const { passwordCheck } = require("../src/passwordChecker");
 
 describe("Password Checker: check if the password is valid", () => {
     it("Password: When length equals to 8", function() {
-        const result = passwordCheck("11111111");
+        const result = passwordCheck("111111&1");
+
         expect(result).toBe(true);
     });
     
     it("Password: When length equals to 7", function() {
-        const result = passwordCheck("1111111");
+        const result = passwordCheck("11&1111");
+
         expect(result).toBe(false);
     });
 
     it("Password: When length equals to 6", function() {
-        const result = passwordCheck("111111");
+        const result = passwordCheck("1&1111");
+
         expect(result).toBe(false);
     });
 
     it("Password: When length equals to 5", function() {
-        const result = passwordCheck("11111");
+        const result = passwordCheck("111&1");
+
+        expect(result).toBe(false);
+    });
+    
+    it("Password: When password doesn't contain any special character", function() {
+        const result = passwordCheck("11111111");
+
         expect(result).toBe(false);
     });
 });
--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,6 +1,6 @@
 function passwordCheck(password) {
-    if(password.length < 8) return false;
-
+    if (password.length < 8) return false;
+    if (!password.includes("&")) return false;
     return true;
 }
 
