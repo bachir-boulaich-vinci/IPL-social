9df314bf0a2a5779ab6551fe3f51e96d294ce823
bachir-boulaich-vinci <bachir.boulaich@student.vinci.be>
2025-01-17 17:02:33 +0100

    refactor: containsNumberChars function

--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,14 +1,21 @@
 function containsSpecialChars(password) {
-    const specialCharacters = /[!@#$%^&*(),.?":{}|<>]/g;
-    return specialCharacters.test(password);
+  const specialCharacters = /[!@#$%^&*(),.?":{}|<>]/g;
+  return specialCharacters.test(password);
+}
+
+function containsNumberChars(password) {
+  const numberCharacters = /[0123456789]/g;
+  return numberCharacters.test(password);
 }
 
 function passwordCheck(password) {
-    if (password.length < 8 || !containsSpecialChars(password)) return false;
-
-    const numberCharacters = /[0123456789]/g;
-    if(!numberCharacters.test(password)) return false;
-    return true;
+  if (
+    password.length < 8 ||
+    !containsSpecialChars(password) ||
+    !containsNumberChars(password)
+  )
+    return false;
+  return true;
 }
 
 module.exports = { passwordCheck };
